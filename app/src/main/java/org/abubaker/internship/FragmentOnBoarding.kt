08efeddadddea167659d1d47e210c0ed9fd269dd
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-27 14:45:43 +0500

    fragment_on_boarding.xml completed

---
+++ app/src/main/java/org/abubaker/internship/FragmentOnBoarding.kt
@@ -0,0 +1,37 @@
+package org.abubaker.internship
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.acxdev.commonFunction.util.view.OtherViewX.Companion.html
+import org.abubaker.internship.databinding.FragmentOnBoardingBinding
+
+class FragmentOnBoarding : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentOnBoardingBinding.inflate(layoutInflater)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        val string =
+            "<font color=#304675>Find</font> <font color=#0DAB4E>Dream <br>Internship</font> <font color=#304675>Program</font>"
+
+        binding.title.html(string)
+
+    }
+
+}
