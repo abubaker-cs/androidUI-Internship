6bd6e6120ffe70e0eef3824a2ff1a09b50359a64
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-27 12:48:29 +0500

    Working...

--- app/src/main/java/org/abubaker/internship/MainActivity.kt
+++ app/src/main/java/org/abubaker/internship/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     // Course is based on
+    // YT: https://www.youtube.com/watch?v=uM2k1O1Xu2w&t=205s&ab_channel=DodyAC
     // https://dribbble.com/shots/15935719/attachments/7771223?mode=media
     // Source: https://github.com/dodyac/InternshipProtoype
 
